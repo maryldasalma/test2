66120d2f2a39b5e6ef40f76b7549b4b9bd2253a2
maryldasalma <135490896+maryldasalma@users.noreply.github.com>
2025-09-24 22:05:41 +0700

    Update master.php

--- test2/master.php
+++ test2/master.php
@@ -53,9 +53,9 @@ if(isset($_POST['simpan'])){
                 <input type="submit" class="btn btn-save" name="simpan" value="SIMPAN">
                 <input type="reset" class="btn btn-cancel" value="BATAL">
             </div>
-            <small>Internal Use Only</small>
         </form>
     </div>
 </div>
 </body>
 </html>
+
